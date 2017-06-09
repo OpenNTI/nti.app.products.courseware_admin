a8bfc1d487ec4b300670839a2bfd567df47d2cbb
Carlos Sanchez <papachoco@gmail.com>
2017-06-09 13:07:50 -0500

    allow view on catalog entries

--- src/nti/app/products/courseware_admin/views/admin_views.py
+++ src/nti/app/products/courseware_admin/views/admin_views.py
@@ -12,12 +12,14 @@ logger = __import__('logging').getLogger(__name__)
 from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
+from pyramid.view import view_defaults
 
 from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.contenttypes.courses.common import get_course_packages
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.contenttypes.courses.sharing import update_package_permissions
 
@@ -29,12 +31,13 @@ ITEMS = StandardExternalFields.ITEMS
 ITEM_COUNT = StandardExternalFields.ITEM_COUNT
 
 
-@view_config(route_name='objects.generic.traversal',
-             renderer='rest',
-             context=ICourseInstance,
-             name='UpdateCourseContentPermissions',
-             request_method='POST',
-             permission=nauth.ACT_NTI_ADMIN)
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='POST',
+               name='UpdateCourseContentPermissions',
+               permission=nauth.ACT_NTI_ADMIN)
 class CourseContentPermissionUpdateView(AbstractAuthenticatedView):
     """
     Update the package permissions for a course.
