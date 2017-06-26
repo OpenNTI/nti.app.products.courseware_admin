47cb70826476b39f2f03a1a5c40745b52bd7cd2b
Carlos Sanchez <papachoco@gmail.com>
2017-06-26 17:22:26 -0500

    move admin views to sync a course

---
+++ src/nti/app/products/courseware_admin/views/sync_views.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import time
+
+from zope import component
+
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+
+from nti.app.contentlibrary.views.sync_views import _SyncAllLibrariesView
+
+from nti.common.string import is_true
+
+from nti.contentlibrary.interfaces import IEditableContentPackage
+
+from nti.contenttypes.courses.common import get_course_packages
+from nti.contenttypes.courses.common import get_course_site_name
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.interfaces import IObjectEntrySynchronizer
+
+from nti.contenttypes.courses.utils import get_course_subinstances
+
+from nti.dataserver import authorization as nauth
+
+from nti.externalization.interfaces import LocatedExternalDict
+from nti.externalization.interfaces import StandardExternalFields
+
+ITEMS = StandardExternalFields.ITEMS
+ITEM_COUNT = StandardExternalFields.ITEM_COUNT
+
+
+@view_config(context=ICourseInstance)
+@view_config(context=ICourseCatalogEntry)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               name='SyncCourse',
+               permission=nauth.ACT_SYNC_LIBRARY)
+class SyncCourseView(_SyncAllLibrariesView):
+
+    def _do_call(self):
+        values = self.readInput()
+        packages = is_true(values.get('packages'))
+        course = ICourseInstance(self.context)
+        entry = ICourseCatalogEntry(self.context)
+        if packages:  # legacy
+            # collect all course associated ntiids
+            ntiids = [entry.ntiid]
+            ntiids.extend(p.ntiid for p in get_course_packages(course)
+                          if not IEditableContentPackage.providedBy(p))
+            ntiids.extend([
+                ICourseCatalogEntry(s).ntiid for s in get_course_subinstances(course)
+            ])
+            # do sync
+            result = self._do_sync(site=get_course_site_name(course),
+                                   ntiids=ntiids,
+                                   allowRemoval=True)
+        else:
+            now = time.time()
+            sync = component.getMultiAdapter((course, course.root),
+                                             IObjectEntrySynchronizer)
+            sync_results = sync.synchronize(course, course.root)
+            result = LocatedExternalDict()
+            result['Results'] = sync_results
+            result['Transaction'] = self._txn_id()
+            result['SyncTime'] = time.time() - now
+        return result
