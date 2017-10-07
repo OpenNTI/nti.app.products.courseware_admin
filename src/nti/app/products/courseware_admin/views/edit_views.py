23a2fa7c2e6eb8a0a3854b44abd0dd47cd95b183
Carlos Sanchez <papachoco@gmail.com>
2017-10-06 21:56:07 -0500

    pylint

--- src/nti/app/products/courseware_admin/views/edit_views.py
+++ src/nti/app/products/courseware_admin/views/edit_views.py
@@ -4,10 +4,9 @@
 .. $Id:
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from pyramid import httpexceptions as hexc
 
@@ -28,6 +27,8 @@ from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 from nti.contenttypes.courses.legacy_catalog import ILegacyCourseCatalogEntry
 
 from nti.dataserver import authorization as nauth
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @view_config(route_name='objects.generic.traversal',
