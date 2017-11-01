6faf7aa48e76448046719b1608c054a6d23f75e6
Carlos Sanchez <papachoco@gmail.com>
2017-11-01 00:03:24 -0500

    add test for presentation assets

--- src/nti/app/products/courseware_admin/tests/test_edit_views.py
+++ src/nti/app/products/courseware_admin/tests/test_edit_views.py
@@ -12,6 +12,14 @@ from hamcrest import is_
 from hamcrest import has_entries
 from hamcrest import assert_that
 
+import os
+import shutil
+import tempfile
+
+import fudge
+
+from nti.cabinet.mixins import SourceFile
+
 from nti.ntiids.ntiids import find_object_with_ntiid
 
 from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
@@ -35,6 +43,13 @@ class TestCourseEdits(ApplicationLayerTest):
     course_path = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323/CourseCatalogEntry'
 
     entry_ntiid = u'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+
+    def presentation_assets_zip(self, tmpdir=None):
+        tmpdir = tmpdir or tempfile.mkdtemp()
+        outfile = os.path.join(tmpdir, "assets")
+        path = os.path.join(os.path.dirname(__file__),
+                            "presentation-assets")
+        return shutil.make_archive(outfile, "zip", path)
 
     @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_course_edit(self):
@@ -69,3 +84,22 @@ class TestCourseEdits(ApplicationLayerTest):
         with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
             entry = find_object_with_ntiid(self.entry_ntiid)
             assert_that(entry.is_locked(), is_(True))
+
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    @fudge.patch('nti.app.contentlibrary.views.bundle_views.get_all_sources',
+                 'nti.app.products.courseware_admin.views.edit_views.save_bundle')
+    def test_presentation_assets(self, mock_src, mock_save):
+        tmpdir = tempfile.mkdtemp()
+        try:
+            path = self.presentation_assets_zip(tmpdir)
+            with open(path, "rb") as fp:
+                source = SourceFile(name="assets.zip", data=fp.read())
+            mock_src.is_callable().returns({"assets.zip":source})
+            mock_save.is_callable().returns(None)
+            
+            res_dict = {}
+            res_dict["title"] = "Another Course"
+            self.testapp.put_json(self.course_path, res_dict, status=200)
+        finally:
+            shutil.rmtree(tmpdir, ignore_errors=True)
